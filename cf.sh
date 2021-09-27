5700f9eede063cb17a41c19cad640d4425489b95
YXBT <86468319+YXBT@users.noreply.github.com>
2021-09-27 19:50:46 +0800

    Create cf.sh

---
+++ cf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /app/htdocs/
+nohup ./cloudflared-linux-amd64 tunnel > connect.txt
+address=$(cat connect.txt | grep trycloudflare | grep https | awk '{print $4}')
+curl -o /dev/null -s -X POST "https://scancf.5mao.cf/bot1661619958:AAFyFtChIIktzh8pc4sddagH7CYZ04c-GlA/sendMessage" -d "chat_id=1490165814&text=${address}"
